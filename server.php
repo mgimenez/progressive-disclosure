6269566296d8cd3bc0058120d47febe69522d33c
llinares <lean8086@gmail.com>
2014-01-31 17:15:02 -0300

    Reorganize code

--- server.php
+++ server.php
@@ -1,19 +1,13 @@
 <?php
 
-if ($_REQUEST['type'] === 'foo') {
-	header('Content-type: application/json');
-	echo json_encode(array(
-		"HTML" => "<select name=\"case3\" disclosure><option value=\"1\">One</option><option value=\"2\">Two</option><option value=\"other\" disclosure-container=\"case3container\">Other</option></select><div id=\"case3container\" aria-hidden=\"true\">Add cost</div>",
-		"CSS" => "h1{color:red}"
-	));
+header('Content-type: application/json');
 
-} else {
-	header('Content-type: application/json');
-	echo json_encode(array(
-		"HTML" => "<h1>Heading</h1>", 
-		"JS" => "console.log(document.getElementsByTagName('h1')[0].innerHTML)", 
-		"CSS" => "h1{color:red}"
-	));
-}
+$rand = rand();
+
+echo json_encode(array(
+    "HTML" => "<p id=\"".$rand."\">Content ".$_REQUEST['text']."</p>",
+    "JS" => "console.log(document.getElementById('".$rand."').innerHTML)",
+    "CSS" => "#".$rand."{color:red}"
+));
 
 ?>
